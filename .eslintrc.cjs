94d7a9caa07866217dd77361b9a812c729e46775
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-10-26 20:30:07 +0100

    :wrench: setup Eslint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  overrides: [
+    { files: ["*.cjs"] },
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ["@typescript-eslint"],
+    },
+  ],
+};
