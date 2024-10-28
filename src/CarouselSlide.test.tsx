2f71f449876b33f156ca19d6ecf4a35af51fe409
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-10-28 09:28:17 +0000

    :construct: Added description and attribution

--- src/CarouselSlide.test.tsx
+++ src/CarouselSlide.test.tsx
@@ -13,4 +13,15 @@ describe('CarouselSlide', () => {
     expect(figure).toContainElement(img);
     expect(figure).toContainElement(figcaption);
   });
+  it('uses `description` and `attribution` as the caption', () => {
+    const props = {
+      description: 'A jaw-droppingly spectacular image',
+      attribution: 'by Johnny Jarecsni',
+    };
+    render(<CarouselSlide {...props} />);
+    const figcaption = screen.getByTestId('caption');
+    expect(figcaption).toHaveTextContent(
+      `${props.description} ${props.attribution}`
+    );
+  });
 });
--- src/CarouselSlide.tsx
+++ src/CarouselSlide.tsx
@@ -1,6 +1,18 @@
-export const CarouselSlide = () => (
+type CarouselProps = {
+  imgUrl?: string;
+  description?: string;
+  attribution?: string;
+};
+export const CarouselSlide = ({
+  imgUrl,
+  description,
+  attribution,
+}: CarouselProps) => (
   <figure>
-    <img />
-    <figcaption data-testid="caption" />
+    <img src={imgUrl} />
+    <figcaption data-testid="caption">
+      <strong>{description}</strong>
+      &nbsp;{attribution}
+    </figcaption>
   </figure>
 );
